b33ba9111c1bf8a0ef64a814ccbfa93f9c58766f
neicedai <37585056+neicedai@users.noreply.github.com>
2024-08-16 08:54:36 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -444,7 +444,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     // TODO: choose the highest balance bus
                     // TODO: Choose a bus
                     let mut bus = rand::thread_rng().gen_range(0..BUS_COUNT);
-                    let mut loaded_config = None;
+                     let mut loaded_config: std::option::Option<T> = None;
                     if let (Ok(_), Ok(config), Ok(busses)) = get_proof_and_config_with_busses(&rpc_client, signer.pubkey()).await {
                     let mut best_bus = 0;
                     for (i, bus) in busses.iter().enumerate() {
