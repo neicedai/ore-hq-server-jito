80c6585504ef94d9a19da35aa5ef73c0afe9713a
neicedai <37585056+neicedai@users.noreply.github.com>
2024-08-16 08:51:51 +0800

    Update ore_utils.rs

--- src/ore_utils.rs
+++ src/ore_utils.rs
@@ -215,6 +215,96 @@ pub fn get_cutoff(proof: Proof, buffer_time: u64) -> i64 {
         .saturating_sub(buffer_time as i64)
         .saturating_sub(now)
 }
+pub async fn get_proof_and_config_with_busses(
+    client: &RpcClient,
+    authority: Pubkey,
+) -> (
+    Result<Proof, ()>,
+    Result<ore_api::state::Config, ()>,
+    Result<Vec<Result<ore_api::state::Bus, ()>>, ()>,
+) {
+    let account_pubkeys = vec![
+        proof_pubkey(authority),
+        CONFIG_ADDRESS,
+        BUS_ADDRESSES[0],
+        BUS_ADDRESSES[1],
+        BUS_ADDRESSES[2],
+        BUS_ADDRESSES[3],
+        BUS_ADDRESSES[4],
+        BUS_ADDRESSES[5],
+        BUS_ADDRESSES[6],
+        BUS_ADDRESSES[7],
+    ];
+    let datas = client.get_multiple_accounts(&account_pubkeys).await;
+    if let Ok(datas) = datas {
+        let proof = if let Some(data) = &datas[0] {
+            Ok(*Proof::try_from_bytes(data.data()).expect("Failed to parse treasury account"))
+        } else {
+            Err(())
+        };
+
+        let treasury_config = if let Some(data) = &datas[1] {
+            Ok(*ore_api::state::Config::try_from_bytes(data.data())
+                .expect("Failed to parse config account"))
+        } else {
+            Err(())
+        };
+        let bus_1 = if let Some(data) = &datas[2] {
+            Ok(*ore_api::state::Bus::try_from_bytes(data.data())
+                .expect("Failed to parse bus1 account"))
+        } else {
+            Err(())
+        };
+        let bus_2 = if let Some(data) = &datas[3] {
+            Ok(*ore_api::state::Bus::try_from_bytes(data.data())
+                .expect("Failed to parse bus2 account"))
+        } else {
+            Err(())
+        };
+        let bus_3 = if let Some(data) = &datas[4] {
+            Ok(*ore_api::state::Bus::try_from_bytes(data.data())
+                .expect("Failed to parse bus3 account"))
+        } else {
+            Err(())
+        };
+        let bus_4 = if let Some(data) = &datas[5] {
+            Ok(*ore_api::state::Bus::try_from_bytes(data.data())
+                .expect("Failed to parse bus4 account"))
+        } else {
+            Err(())
+        };
+        let bus_5 = if let Some(data) = &datas[6] {
+            Ok(*ore_api::state::Bus::try_from_bytes(data.data())
+                .expect("Failed to parse bus5 account"))
+        } else {
+            Err(())
+        };
+        let bus_6 = if let Some(data) = &datas[7] {
+            Ok(*ore_api::state::Bus::try_from_bytes(data.data())
+                .expect("Failed to parse bus6 account"))
+        } else {
+            Err(())
+        };
+        let bus_7 = if let Some(data) = &datas[8] {
+            Ok(*ore_api::state::Bus::try_from_bytes(data.data())
+                .expect("Failed to parse bus7 account"))
+        } else {
+            Err(())
+        };
+        let bus_8 = if let Some(data) = &datas[9] {
+            Ok(*ore_api::state::Bus::try_from_bytes(data.data())
+                .expect("Failed to parse bus1 account"))
+        } else {
+            Err(())
+        };
+
+        (proof, treasury_config, Ok(vec![bus_1, bus_2, bus_3, bus_4, bus_5, bus_6, bus_7, bus_8]))
+    } else {
+        (Err(()), Err(()), Err(()))
+    }
+}
+
+
 
 pub fn find_hash_par(proof: Proof, cutoff_time: u64, threads: u64, min_difficulty: u32, mining_messages_reciever: Receiver<MiningDataChannelMessage>, mining_messages_sender: Sender<MiningDataChannelMessage>) -> (Solution, u32, Hash, u64) {
     let handles = (0..threads)
