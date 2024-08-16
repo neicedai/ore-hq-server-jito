1084502c9bc6ae3ac55fd8a5c020bb1848f6c0b7
neicedai <37585056+neicedai@users.noreply.github.com>
2024-08-16 08:18:11 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -442,7 +442,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     ixs.push(noop_ix);
 
                     // TODO: choose the highest balance bus
-                    let bus = rand::thread_rng().gen_range(0..BUS_COUNT);
+                    // TODO: Choose a bus
+let mut bus = rand::thread_rng().gen_range(0..BUS_COUNT);
+let mut loaded_config = None;
+if let (Ok(_), Ok(config), Ok(busses)) = get_proof_and_config_with_busses(&rpc_client, signer.pubkey()).await {
+    let mut best_bus = 0;
+    for (i, bus) in busses.iter().enumerate() {
+        if let Ok(bus) = bus {
+            if bus.rewards > busses[best_bus].unwrap().rewards {
+                best_bus = i;
+            }
+        }
+    }
+    bus = best_bus;
+}
+
                     let difficulty = solution.to_hash().difficulty();
 
                     let ix_mine = get_mine_ix(signer.pubkey(), solution, bus);
